67a8fefb35b3f061019efafae9e7d58117571623
ap2k3 <arish2k3@gmail.com>
2023-06-17 14:02:18 +0530

    update

---
+++ atlas_uri.js
@@ -0,0 +1,2 @@
+module.exports = uri =
+    "mongodb+srv://admin:adminpassword@cluster0.kvmgyqe.mongodb.net/?retryWrites=true&w=majority";
---
+++ index.js
@@ -0,0 +1,18 @@
+const { MongoClient } = require("mongodb");
+const uri = require("./atlas_uri");
+
+console.log(uri);
+
+const client = new MongoClient(uri);
+const dbname = "bank";
+
+const connectToDatabase = async () => {
+    try {
+        await client.connect();
+        console.log(`Connected to the ${dbname} database.`);
+    } catch (err) {
+        console.error(`Error connecting to the database: ${err}`);
+    }
+};
+
+connectToDatabase();
