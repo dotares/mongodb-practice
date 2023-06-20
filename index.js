e3ea1b8e4b3b7aa956f254011426345ba1ffc83e
ap2k3 <arish2k3@gmail.com>
2023-06-20 14:41:05 +0530

    update

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 const { MongoClient, ServerApiVersion } = require("mongodb");
+
 const uri =
     "mongodb+srv://admin:adminpassword@testcluster.9hkrkub.mongodb.net/?retryWrites=true&w=majority";
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    },
+});
+
+async function run() {
+    try {
+        await client.connect();
+
+        await client.db("admin").command({ ping: 1 });
+
+        console.log("Pinged your deployment. You are now connected");
+    } finally {
+        await client.close();
+    }
+}
+
+run().catch(console.dir);
