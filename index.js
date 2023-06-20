9e8eb8ed31f9fe81405f4410759314b5fb26c249
ap2k3 <arish2k3@gmail.com>
2023-06-20 16:35:01 +0530

    update

--- index.js
+++ index.js
@@ -12,8 +12,10 @@ const client = new MongoClient(uri, {
 });
 
 const dbName = "stationary";
+const collectionName = "inventory";
+const accountsCollection = client.db(dbName).collection(collectionName);
 
-const sampleDocument = {
+const sampleStationary = {
     item: "pencil",
     qty: 10,
     tags: ["red", "black", "thin"],
@@ -23,10 +25,12 @@ const sampleDocument = {
 async function run() {
     try {
         await client.connect();
+        let result = await accountsCollection.insertOne(sampleStationary);
         await client.db(dbName).command({ ping: 1 });
         console.log(
             `Pinged your deployment (${dbName}). You are now connected`
         );
+        console.log(result.insertedId);
     } finally {
         await client.close();
     }
