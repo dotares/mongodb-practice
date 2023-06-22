0452e1dbf21d82627564b569b0356d1e3b8c03ab
ap2k3 <arish2k3@gmail.com>
2023-06-22 13:31:58 +0530

    update

--- index.js
+++ index.js
@@ -24,11 +24,36 @@ const connectToDb = async () => {
     }
 };
 
+const pipeline = [
+    // Stage 1: match the accounts with a balance greater than $1,000
+    {
+        $match: {
+            balance: {
+                $lt: 1000,
+            },
+        },
+    },
+    // Stage 2: calculate average balance and total balance
+    {
+        $group: {
+            _id: "$account_type",
+            total_balance: {
+                $sum: "$balance",
+            },
+            avg_balance: {
+                $avg: "$balance",
+            },
+        },
+    },
+];
+
 const run = async () => {
     try {
         await connectToDb().catch(console.dir);
-        let result = await accountsCollection.find().toArray();
-        console.log(result);
+        let result = await accountsCollection.aggregate(pipeline);
+        for await (const doc of result) {
+            console.log(doc);
+        }
     } catch (err) {
         console.error(`Error: ${err}`);
     } finally {
