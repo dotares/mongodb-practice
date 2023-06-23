f2dcd5ceb705ae0d22d4b5bdad9d3f0014a937c6
ap2k3 <arish2k3@gmail.com>
2023-06-23 09:23:07 +0530

    update

--- index.js
+++ index.js
@@ -3,13 +3,7 @@ require("dotenv").config();
 
 const uri = process.env.ATLAS_URI;
 
-const client = new MongoClient(uri, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-    },
-});
+const client = new MongoClient(uri);
 
 const dbName = "";
 const collectionName = "";
